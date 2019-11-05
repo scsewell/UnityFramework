469441ca945dfc19439de1fd00c62515d5282fd5
scsewell <scott.sewell@mail.mcgill.ca>
2019-11-05 01:04:58 -0500

    Add serializable Guid type

---
+++ Scripts/Utils/DataTypes/UnityGuid.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+using UnityEngine;
+
+namespace Framework
+{
+    /// <summary>
+    /// A Guid that can be serialized by Unity.
+    /// </summary>
+    [Serializable]
+    [StructLayout(LayoutKind.Explicit)]
+    public struct UnityGuid : IComparable, IComparable<Guid>, IEquatable<Guid>
+    {
+        [FieldOffset(0)]
+        private Guid m_guid;
+
+        [SerializeField]
+        [FieldOffset(0)]
+        private long m_part0;
+
+        [SerializeField]
+        [FieldOffset(8)]
+        private long m_part1;
+
+        public int CompareTo(object obj)
+        {
+            if (obj is UnityGuid unityGuid)
+            {
+                return m_guid.CompareTo(unityGuid.m_guid);
+            }
+            if (obj is Guid guid)
+            {
+                return m_guid.CompareTo(guid);
+            }
+            return -1;
+        }
+
+        public int CompareTo(Guid other)
+        {
+            return m_guid.CompareTo(other);
+        }
+
+        public bool Equals(Guid other)
+        {
+            return m_guid == other;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is UnityGuid unityGuid)
+            {
+                return unityGuid == m_guid;
+            }
+            if (obj is Guid guid)
+            {
+                return guid == m_guid;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return m_guid.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return m_guid.ToString();
+        }
+
+        public static implicit operator Guid(UnityGuid uGuid) => uGuid.m_guid;
+    }
+}
---
+++ Scripts/Utils/Editor/DataTypes/UnityGuidDrawer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+using UnityEngine;
+using UnityEditor;
+
+using Framework.EditorTools;
+
+namespace Framework
+{
+    [CustomPropertyDrawer(typeof(UnityGuid))]
+    public class UnityGuidDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            // get all the guid values
+            object[] guids = SerializedObjectUtils.GetPropertyField(property);
+
+            // display the guid value
+            label = EditorGUI.BeginProperty(position, label, property);
+            position = EditorGUI.PrefixLabel(position, label);
+
+            const float BUTTON_WIDTH = 50f;
+
+            if (!property.hasMultipleDifferentValues)
+            {
+                UnityGuid guid = (UnityGuid)guids.FirstOrDefault();
+
+                Rect labelRect = position;
+                labelRect.xMax = position.xMax - BUTTON_WIDTH;
+
+                EditorGUI.LabelField(position, guid.ToString());
+
+                Rect buttonRect = position;
+                buttonRect.xMin = position.xMax - BUTTON_WIDTH;
+                
+                // generate a new guid if the current value is not defined or if we want a new one
+                if (GUI.Button(buttonRect, "New") || guid == Guid.Empty)
+                {
+                    byte[] bytes = Guid.NewGuid().ToByteArray();
+
+                    property.FindPropertyRelative("m_part0").longValue = BitConverter.ToInt64(bytes, 0);
+                    property.FindPropertyRelative("m_part1").longValue = BitConverter.ToInt64(bytes, 8);
+                }
+            }
+
+            EditorGUI.EndProperty();
+        }
+    }
+}
